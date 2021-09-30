13663e0bc85d84a5462b1446eb764081fb93a75d
Nilay Baranwal <65295315+nilayb12@users.noreply.github.com>
2021-09-30 18:50:08 +0530

    Minute Hand updates per second

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ setInterval(() => {
     let ss = day.getSeconds() * deg;
     
     hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
-    mn.style.transform = `rotateZ(${mm}deg)`;
+    mn.style.transform = `rotateZ(${(mm)+(ss/60)}deg)`;
     sc.style.transform = `rotateZ(${ss}deg)`;
 });
